02943d7663598b303d15081669201905dd4081de
Ankit Ydv <ankityadav11241@gmail.com>
2024-06-18 00:35:21 +0530

    dashboard ui

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+import Link from "next/link";
+import BlogTable from "./components/BlogTable";
+
 export default function Dashboard() {
-  return <div>page</div>;
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Blogs</h1>
+        <Link href="/dashboard/blog/create">
+          <Button variant={"outline"}>
+            Create <PlusIcon />
+          </Button>
+        </Link>
+      </div>
+      <BlogTable />
+    </div>
+  );
 }
