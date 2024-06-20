fe87e818b26f073cd4cda88dd5cdb5303c375fc7
Ankit Ydv <ankityadav11241@gmail.com>
2024-06-20 10:55:25 +0530

    fix: supabase bug

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
-import { Database } from "@/lib/types/supabase";
+// import { Database } from "@/lib/types/supabase";
 
 export async function GET(request: Request) {
 	const requestUrl = new URL(request.url);
@@ -17,7 +17,7 @@ export async function GET(request: Request) {
 
 	if (code) {
 		const cookieStore = cookies();
-		const supabase = createServerClient<Database>(
+		const supabase = createServerClient(
 			process.env.NEXT_PUBLIC_SUPABASE_URL!,
 			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 			{
