53e61c24de1b806d68102ba501bd35ecb9d485ff
Ankit Ydv <ankityadav11241@gmail.com>
2024-06-22 17:39:44 +0530

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,50 @@
+import Footer from "@/components/Footer";
 import { readBlogs } from "@/lib/actions/blog";
 import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
-  
   const { data: blogs } = await readBlogs();
-  return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-5 p-5 xl:p-0">
-      {blogs?.map((blog, index) => {
-        return (
-          <Link
-            href={"/blog/" + blog.id}
-            className="w-full  border rounded-md dark:bg-graident-dark p-5 hover:ring-2 ring-green-500 transition-all cursor-pointer space-y-5 first:lg:col-span-2 first:md:col-span-3"
-            key={index}>
-            <div className="w-full h-72 sm:w-full  md:h-64 xl:h-96  relative">
-              <Image
-                priority
-                src={blog.image_url}
-                alt="cover"
-                fill
-                className=" rounded-md object-cover object-center"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              />
-            </div>
-            <div className="space-y-2">
-              <p className="text-sm dark:text-gray-400">
-                {new Date(blog.created_at).toDateString()}
-              </p>
 
-              <h1 className="text-xl font-bold dark:text-gray-300">
-                {blog.title}
-              </h1>
-            </div>
-          </Link>
-        );
-      })}
-    </div>
+  return (
+    <>
+      <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-5 p-5 xl:p-0 min-h-[50dvh]">
+        {blogs?.length === 0 && (
+          <div className="w-full col-span-3 h-72 sm:w-full  md:h-64 xl:h-96  relative flex justify-center items-center flex-col">
+            <div className="text-lg sm:text-3xl font-semibold text-nowrap">No blogs to show !</div>
+            <div className="text-md sm:text-xl mt-4">Create one to view</div>
+          </div>
+        )}
+        {blogs?.map((blog, index) => {
+          return (
+            <Link
+              href={"/blog/" + blog.id}
+              className="w-full  border rounded-md dark:bg-graident-dark p-5 hover:ring-2 ring-green-500 transition-all cursor-pointer space-y-5 first:lg:col-span-2 first:md:col-span-3"
+              key={index}>
+              <div className="w-full h-72 sm:w-full  md:h-64 xl:h-96  relative">
+                <Image
+                  priority
+                  src={blog.image_url}
+                  alt="cover"
+                  fill
+                  className=" rounded-md object-cover object-center"
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                />
+              </div>
+              <div className="space-y-2">
+                <p className="text-sm dark:text-gray-400">
+                  {new Date(blog.created_at).toDateString()}
+                </p>
+
+                <h1 className="text-xl font-bold dark:text-gray-300">
+                  {blog.title}
+                </h1>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+      <Footer />
+    </>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import Link from "next/link";
+import { BsGithub, BsLinkedin, BsTwitterX } from "react-icons/bs";
 
 export default function Footer() {
+  const links = [
+    {
+      name: "GitHub",
+      link: "https://github.com/ydv-ankit",
+      Icon: <BsGithub />,
+    },
+    {
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/in/ankityadav1121/",
+      Icon: <BsLinkedin />,
+    },
+    {
+      name: "X.com",
+      link: "https://x.com/ydvtwts",
+      Icon: <BsTwitterX />,
+    },
+  ];
   return (
-    <div>Footer</div>
-  )
+    <div className="w-full min-h-20 border-t border-b">
+      <div className="flex flex-col items-center justify-between h-full p-4 sm:flex-row">
+        <div className="flex justify-center items-center">
+          <div className="">
+            <div className="text-xl font-bold my-2">
+              PenPulse - <span className="text-xl font-medium">Ankit Ydv</span>
+            </div>
+            <div className="text-md opacity-65">
+              &copy; {new Date().getFullYear()} - All rights reserved
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-start mt-4 sm:mt-0">
+          {links.map(({ name, link, Icon }, index) => (
+            <Link href={link} key={index} target="_blank">
+              <div className="flex justify-center items-center hover:underline transition-all rounded-md">
+                {Icon}
+                <div className="mx-2 my-1">{name}</div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
